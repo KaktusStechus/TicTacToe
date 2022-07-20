b794c2d795fd6879d10ec41d0dc648a3517a1592
KaktusStechus <nikita1.duemmel@uni-bayreuth.de>
2022-07-20 18:35:42 +0200

    added one very important comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
 
+	/*
+	 * This is the Main class with the main method... yay!
+	 */
     public static void main(String[] args) {
     	Logic logic = new LogicTicTacToe();
     	GUI gui = new GUI(logic);
